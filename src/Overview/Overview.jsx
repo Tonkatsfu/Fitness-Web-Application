6fe951bcc640cf2b306b8c222b151ea78218278a
LapasaranJoemar <joemar_lapasaran_a@dlsu.edu.ph>
2024-08-25 20:56:26 +0800

    updated overview icons

--- src/Overview/Overview.jsx
+++ src/Overview/Overview.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Overview.module.css';
 
 function Overview() {
+    const [activeItem, setActiveItem] = useState('Dashboard');
+
     return (
         <div className={styles.mainContainer}>
             <div className={styles.leftContainer}>
@@ -15,15 +17,24 @@ function Overview() {
                 </div>
 
                 <div className={styles.navButtons}>
-                    <div className={styles.navItem1}>
+                    <div 
+                        className={`${styles.navItem} ${activeItem === 'Dashboard' ? styles.active : ''}`}
+                        onClick={() => setActiveItem('Dashboard')}
+                    >
                         <img src='src/assets/icons/dashboard.png' alt="Dashboard" className={styles.dashboard} />
                         <p className={styles.dashboardTitle}>Dashboard</p>
                     </div>
-                    <div className={styles.navItem2}>
+                    <div 
+                        className={`${styles.navItem} ${activeItem === 'Macros' ? styles.active : ''}`}
+                        onClick={() => setActiveItem('Macros')}
+                    >
                         <img src='src/assets/icons/calorie.png' alt="Calorie" className={styles.dashboard} />
                         <p className={styles.dashboardTitle}>Macros</p>
                     </div>
-                    <div className={styles.navItem3}>
+                    <div 
+                        className={`${styles.navItem} ${activeItem === 'Weekly Planner' ? styles.active : ''}`}
+                        onClick={() => setActiveItem('Weekly Planner')}
+                    >
                         <img src='src/assets/icons/weeklyPlanner.png' alt="WeeklyPlanner" className={styles.dashboard} />
                         <p className={styles.dashboardTitle}>Weekly Planner</p>
                     </div>
@@ -31,7 +42,13 @@ function Overview() {
             </div>
 
             <div className={styles.rightContainer}>
-                
+                <div className={styles.searchBar}>
+
+                </div>
+
+                <div className={styles.rightSideContainer}>
+                    
+                </div>
             </div>
         </div>
     );
